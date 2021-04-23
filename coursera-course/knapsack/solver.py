5710896f29182fafde201cd468e124aea5438392
bizon <curry.john@gmail.com>
2021-04-23 09:48:59 +0100

    just starting to add brute force option to solver

--- coursera-course/knapsack/solver.py
+++ coursera-course/knapsack/solver.py
@@ -3,20 +3,60 @@
 
 from collections import namedtuple
 Item = namedtuple("Item", ['index', 'value', 'weight'])
+import numpy as np
 
-def loop_options():
-    #do my loop with n iterations, flipping and copying
-
-    return options
 
 def recurse_options():
     #https://www.geeksforgeeks.org/generate-all-the-binary-strings-of-n-bits/
 
     return options
 
-def brute_force():
+def loop_options():
+    #do my loop with n iterations, flipping and copying
 
-    return answer
+    return options
+
+
+def brute_force(taken, items, capacity):
+    n = len(taken)
+    perms = np.zeros(1, n)
+    for i in range (0, n-1):
+        tmp = np.copy(perms)
+        tmp[:i] = 1
+        perms = np.append(perms, tmp, axis = 0)
+    
+    print(perms)
+
+
+
+    ##generate sequence of zeros of length n: s
+    ##generate table of options
+    #for i = 1: n:
+        #copy s
+        #i col = 1
+        #append to s
+    #multiply the binaries by their weights/values to get and weight/value total per rows
+    #get row with max value where max weight >= capacity
+    #if not already, convert it to a list and return it along with total weight and value for it
+
+    #return taken, value, weight
+
+def naive(taken, items, capacity):
+
+    # a trivial algorithm for filling the knapsack
+    # it takes items in-order until the knapsack is full
+
+    value = 0
+    weight = 0
+
+    for item in items:
+        if weight + item.weight <= capacity:
+            taken[item.index] = 1
+            value += item.value
+            weight += item.weight
+
+    return taken, value, weight
+
 
 
 
@@ -36,18 +76,12 @@ def solve_it(input_data):
         line = lines[i]
         parts = line.split()
         items.append(Item(i-1, int(parts[0]), int(parts[1])))
-
-    # a trivial algorithm for filling the knapsack
-    # it takes items in-order until the knapsack is full
-    value = 0
-    weight = 0
+ 
     taken = [0]*len(items)
 
-    for item in items:
-        if weight + item.weight <= capacity:
-            taken[item.index] = 1
-            value += item.value
-            weight += item.weight
+    taken, value, weight = naive(taken, items, capacity)
+
+    brute_force(taken, items, capacity)
     
     # prepare the solution in the specified output format
     output_data = str(value) + ' ' + str(0) + '\n'
